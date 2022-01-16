658cc8127a04c94dd14733a8fc7d3f30a4f5d735
j-aguil <jagui105@calpoly.edu>
2022-01-15 17:05:33 -0800

    Finished part 5

--- backend.js
+++ backend.js
@@ -1,49 +1,86 @@
 const express = require('express');
-const res = require('express/lib/response');
 const app = express();
 const port = 5000;
 
-const users = {
-    users_list : 
+const users = { 
+    users_list :
     [
-        {
-            id: 'xyz789',
-            name: 'Charlie',
-            job: 'Janitor',
-        },
-        {
-            id: 'abc123',
-            name: 'Mac',
-            job: 'Bouncer',
-        },
-        {
-            id: 'ppp222',
-            name: 'Mac',
-            job: 'Professor',
-        },
-        {
-            id: 'yat999',
-            name: 'Dee',
-            job: 'Aspiring actress',
-        },
-        {
-            id: 'zap555',
-            name: 'Dennis',
-            job: 'Bartender',
-        },
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
     ]
-}
+ }
+
 
 app.use(express.json());
 
 // app.get('/', (req, res) => {
-//     res.send('Hello World!')
+//     res.send('Hello World!');
 // });
 
-app.get('/users', (req, res) => {
-    res.send(users);
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if(result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
 });
 
-app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+function findUserById(id){
+    return users['users_list'].find( (user) => user['id'] === id); //or line below
+    //return users['users_list'].filter( (user) => user['id] === id) since user id in this case is unique
+} 
+
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+//
+
+// app.get('/users', (req, res) => {
+//     res.send(users);
+// });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});      
+
+
